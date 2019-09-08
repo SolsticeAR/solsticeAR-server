0314f91e16f22b8421789f0aa1a6e40fbd84cdca
Wil Andrade and julian <andradewilker+j.mourell@gmail.com>
2019-09-08 16:37:41 +0900

    Fix context bug that breaks server if token is not passed in the request.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const { ApolloServer, AuthenticationError } = require("apollo-server");
 const { AdminAPI } = require("./datasources");
 const resolvers = require("./resolvers");
 const typeDefs = require("./typedefs");
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 /**
-  * Static jwt authentication function
-*/
+ * Static jwt authentication function
+ */
 const context = ({ req }) => {
   const token = req.headers.authorization || "";
+  if (!token) return null;
   const splitToken = token.split(" ")[1];
   const jwtVerify = jwt.verify(splitToken, "temp_secret");
   console.log(jwtVerify);
