97ee7c05a5d65f3cec1308bf0dc04c632f2ee239
Lucas <kimlucas@gmail.com>
2019-09-17 16:51:32 +0900

    Successfully deployed to heroku

--- models/index.js
+++ models/index.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || "development";
 const env = process.env.DATABASE_URL ? "production" : "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
