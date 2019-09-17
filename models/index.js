8b65c91541d50bc69d1d0d77f72ac9626ff7774d
Lucas <kimlucas@gmail.com>
2019-09-17 12:25:44 +0900

    deploying to heroku

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
-"use strict";
+// dotenv
+require("dotenv").config();
 
 const fs = require("fs");
 const path = require("path");
@@ -11,14 +12,8 @@ const db = {};
 let sequelize;
 // if (config.use_env_variable) {
 //   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-if (process.env.HEROKU_POSTGRESQL_WHITE_URL) {
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_WHITE_URL, {
-    dialect: "postgres",
-    protocol: "postgres",
-    port: match[4],
-    host: match[3],
-    logging: true //false
-  });
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   sequelize = new Sequelize(
     config.database,
