aabb94ae1c8d6bd842e5e16e0de322fea9d6c5c1
wilandrade <wilandrade@Wils-MacBook-Pro.local>
2019-09-17 17:04:40 +0900

    Remove console logs

--- resolvers/index.js
+++ resolvers/index.js
@@ -20,12 +20,10 @@ const resolvers = {
       const campaigns = await dataSources.campaignAPI.getAdminCampaigns(
         adminID
       );
-      console.log(campaigns);
       return campaigns;
     },
-    getCampaigns: async (_, {}, { dataSources }) => {
+    getCampaigns: async (_, { }, { dataSources }) => {
       const campaigns = await dataSources.campaignAPI.getCampaigns();
-      console.log(campaigns);
       return campaigns;
     }
   },
