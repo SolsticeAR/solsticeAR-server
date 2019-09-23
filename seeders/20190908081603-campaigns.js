73ee75979b5b4c9a26c140fb11762de993afaef3
lucas and Wil Andrade <kimlucas92+andradewilker@gmail.com>
2019-09-23 15:15:06 +0900

    added more seed data to live database

--- seeders/20190908081603-campaigns.js
+++ seeders/20190908081603-campaigns.js
@@ -3,58 +3,66 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("campaigns", [
+      // many comments because we wanted to seed new data without compromising the live database
+      // {
+      //   name: "Test Campaign 1",
+      //   admin_id: 1,
+      //   active: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name: "Test Campaign 2",
+      //   admin_id: 1,
+      //   active: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name: "Test Campaign 3",
+      //   admin_id: 1,
+      //   active: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name: "Test Campaign 1",
+      //   admin_id: 2,
+      //   active: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name: "DEMO IMAGE",
+      //   admin_id: 4,
+      //   active: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name: "DEMO VIDEO",
+      //   admin_id: 4,
+      //   active: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name: "DEMO GIF",
+      //   admin_id: 4,
+      //   active: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // },
+      // {
+      //   name: "DEMO TEXT",
+      //   admin_id: 4,
+      //   active: true,
+      //   createdAt: new Date(),
+      //   updatedAt: new Date()
+      // }
       {
-        name: "Test Campaign 1",
-        admin_id: 1,
-        active: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: "Test Campaign 2",
-        admin_id: 1,
-        active: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: "Test Campaign 3",
-        admin_id: 1,
-        active: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: "Test Campaign 1",
-        admin_id: 2,
-        active: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: "DEMO IMAGE",
-        admin_id: 4,
-        active: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: "DEMO VIDEO",
-        admin_id: 4,
-        active: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: "DEMO GIF",
-        admin_id: 4,
-        active: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        name: "DEMO TEXT",
-        admin_id: 4,
+        name: "Hulk Hogan",
+        admin_id: 6,
         active: true,
         createdAt: new Date(),
         updatedAt: new Date()
